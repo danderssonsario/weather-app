52b2a74110890d8226e30adf96d7cde2892a5cbc
Daniel Andersson <da222xg@student.lnu.se>
2024-01-12 22:19:32 +0100

    Add fixes.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,51 +1,51 @@
 import { useState, ChangeEvent, FormEvent } from 'react'
 import { Input, Button } from '@nextui-org/react'
 
- interface LocationData {
-    main: {
-      temp: number
-    }
+interface LocationData {
+  main: {
+    temp: number
   }
+}
 
-  interface Props {
-    onSubmitData: (data: LocationData) => void
-  }
+interface Props {
+  onSubmitData: (data: LocationData) => void
+}
 
-function SearchForm({ onSubmitData } : Props) {
-
-  const [input, setInput] = useState<string>('');
+function SearchForm({ onSubmitData }: Props) {
+  const [input, setInput] = useState<string>('')
 
   const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      const [latitude, longitude] = await fetchCoordinates();
+      const [latitude, longitude] = await fetchCoordinates()
 
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
-      );
-      const data: LocationData = await response.json();
+      )
+      const data: LocationData = await response.json()
 
-      onSubmitData(data); // Call the parent's callback with the data
+      onSubmitData(data)
     } catch (error) {
-      console.log(error);
+      console.error(error)
     }
-  };
+  }
 
   const fetchCoordinates = async (): Promise<[number, number]> => {
-    const response = await fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${import.meta.env.VITE_API_KEY}`
-    );
-    const data: { lat: number; lon: number }[] = await response.json();
-    return [data[0].lat, data[0].lon];
-  };
+      const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${import.meta.env.VITE_API_KEY}`)
+      const data: { lat: number; lon: number }[] = await response.json()
+      return [data[0].lat, data[0].lon]
+  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  };
+    setInput(e.target.value)
+  }
 
   return (
     <>
-      <form className='flex flex-row' onSubmit={handleSubmit}>
+      <form
+        className='flex flex-row'
+        onSubmit={handleSubmit}
+      >
         <Input
           size='sm'
           type='text'
@@ -53,12 +53,16 @@ function SearchForm({ onSubmitData } : Props) {
           placeholder='Location'
           onChange={handleChange}
         />
-        <Button size='lg' type='submit' color='secondary'>
+        <Button
+          size='lg'
+          type='submit'
+          color='secondary'
+        >
           Button
         </Button>
       </form>
     </>
-  );
+  )
 }
 
 export default SearchForm
